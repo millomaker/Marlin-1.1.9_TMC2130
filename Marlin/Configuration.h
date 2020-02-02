75f7f30538434181f39f664bb5b5edd004c59be4
millomaker <bj.millot24@gmail.com>
2020-02-02 19:23:08 +0100

    Change step per unit E : 140
    Changed step per unit of Extruder to 140

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -609,7 +609,7 @@
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  X : 32; Y : 16; Z : ?;E : ?;*/
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 146 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 140 }
 
 /**
  * Default Max Feed Rate (mm/s)
